c2dddb1c91ec575590e74e95f25ee31e2ecca526
Jason Lind <lind@yahoo.com>
2017-04-21 20:42:21 -0500

    Query implementation

--- Lind.Euros.Client/Query.cs
+++ Lind.Euros.Client/Query.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lind.Euros.Client
 {
     public interface IQuery : IContext<UserMessage>
     {
         User OtherUser { get; }
+    }
+    public class Query : Context<UserMessage>, IQuery
+    {
+        public Query(IServer server, ICommandFactory commandFactory, User user)
+            :base(server, commandFactory)
+        {
+            this.OtherUser = user;
+        }
+        public User OtherUser { get; private set; }
+
+        public override Task SendMessage(string message, CancellationToken token = default(CancellationToken))
+        {
+            return this.Server.SendCommand(this.CommandFactory.Create(message, this.OtherUser.Nick), token);
+        }
     }
 }
